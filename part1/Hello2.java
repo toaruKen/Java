f1a22df378a315e564d6e4ee830d235ee636c89f
toaruKen <cruiser5.festiva1@gmail.com>
2019-02-18 15:14:23 +0900

    part1 end

---
+++ part1/Hello2.java
@@ -0,0 +1,6 @@
+class Hello2 {
+    public static void main (String[] args) {
+        System.out.println("もうかってまっか");
+        System.out.println("ぼちぼちでんがな");
+    }
+}
---
+++ part1/Hello3.java
@@ -0,0 +1,6 @@
+class Hello3 {
+    public static void main (String [] args){
+        System.out.println("じゃばで");
+        System.out.println("行こう");
+    }
+}
